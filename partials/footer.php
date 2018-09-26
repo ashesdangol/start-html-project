8a0dcc9b5125496aa49ea4155ee6169f7df0ba06
ashesdangol <ashesdangol@hotmail.com>
2018-09-26 22:45:58 +0545

    complete working structure, no bugs, no navigation

--- partials/footer.php
+++ partials/footer.php
@@ -3,15 +3,11 @@
   <!-- jQuery local fallback -->
   <script>window.jQuery || document.write('<script src="js/jquery-3.3.1.js"><\/script>')</script>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/popper.min.js"></script>
-  <script>if(typeof(Popper) === 'undefined') {document.write('<script src="bs_js/popper.min.js"><\/script>')}</script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 
-  <!--bootstrap js cdn  -->
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  <!-- Bootstrap JS local fallback -->
-  <script>if(typeof($.fn.modal) === 'undefined') {document.write('<script src="bs_js/bootstrap.min.js"><\/script>')}</script>
+  <script>if(typeof(Popper) === 'undefined' || typeof($.fn.modal) === 'undefined') {document.write('<script src="bs_js/bootstrap.bundle.min.js"><\/script>')}</script>
 
-
+  
   <!--If bootstrap cdn is unavialble use local boostrap css -->
   <div id="bootstrapCssTest" class="d-none"></div>
   <script>
--- partials/header.php
+++ partials/header.php
@@ -3,8 +3,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap-reboot.css.map" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap-reboot.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap-reboot.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
+
+
     <title>New Web Page</title>
   </head>
   <body>
