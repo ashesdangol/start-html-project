feb5bf2814aaefa574073939e1fb5acf99912c69
ashesdangol <ashesdangol@hotmail.com>
2018-09-28 15:34:50 +0545

    complete nav with animation

--- js/custom.js
+++ js/custom.js
@@ -1,45 +1,50 @@
 $(document).ready(function(){
-  $('nav').on('click','.navbar-toggler', function(){
-    $(this).closest('nav').find('#navbarNav').toggleClass('open-nav');
-    $(this).toggleClass('x');
-    $('body').toggleClass('hide-overflow');
-  });
+  function basic_navigation(){
+    $('nav').on('click','.navbar-toggler', function(){
+      $(this).closest('nav').find('#navbarNav').toggleClass('open-nav');
+      $(this).toggleClass('x');
+      $('body').toggleClass('hide-overflow');
+    });
 
-  $('nav').on('click','.nav-item', function(){
-    $(this).closest('.navbar-nav').find('.active').removeClass('active');
-    $(this).addClass('active');
-  });
+    $('nav').on('click','.nav-item', function(){
+      $(this).closest('.navbar-nav').find('.active').removeClass('active');
+      $(this).addClass('active');
+    });
 
-  $('nav').on('click','.nav-item', function(){
-    var ashes=$('.nav_main--wrapper').find('.open-nav');
-    if(ashes.length>0){
-      $( ".navbar-toggler" ).trigger( "click" );
-    }
-  });
+    $('nav').on('click','.nav-item', function(){
+      var ashes=$('.nav_main--wrapper').find('.open-nav');
+      if(ashes.length>0){
+        $( ".navbar-toggler" ).trigger( "click" );
+      }
+    });
 
-  window.addEventListener('resize', function () {
-    var docSize=$(document).width();
-    if(docSize>991){
-      $('nav').find('#navbarNav').removeClass('open-nav');
-      $('nav').find('.navbar-toggler').removeClass('x');
-      $('body').removeClass('hide-overflow');
-    }
-  });
-  var lastScrollPosition=0;
-  $(document).on('scroll', function(){
-    var currentScrollPosition=$(this).scrollTop();
-    if(currentScrollPosition > lastScrollPosition){
+    window.addEventListener('resize', function () {
+      var docSize=$(document).width();
+      if(docSize>991){
+        $('nav').find('#navbarNav').removeClass('open-nav');
+        $('nav').find('.navbar-toggler').removeClass('x');
+        $('body').removeClass('hide-overflow');
+      }
+    });
+    var lastScrollPosition=0;
+    $(document).on('scroll', function(){
+      var currentScrollPosition=$(this).scrollTop();
+      if(currentScrollPosition > lastScrollPosition){
 
-      $(this).find('nav').removeClass('show-this');
-      $(this).find('nav').addClass('hide-this');
-      $(this).find('body').addClass('hide-padding');
+        $(this).find('nav').removeClass('show-this');
+        $(this).find('nav').addClass('hide-this');
+        $(this).find('body').addClass('hide-padding');
 
-    }else{
-      $(this).find('body').removeClass('hide-padding');
-      $(this).find('nav').removeClass('hide-this');
-      $(this).find('nav').addClass('show-this');
-    }
-    lastScrollPosition=currentScrollPosition;
-  })
+      }else{
+        $(this).find('body').removeClass('hide-padding');
+        $(this).find('nav').removeClass('hide-this');
+        $(this).find('nav').addClass('show-this');
+      }
+      lastScrollPosition=currentScrollPosition;
+    })
+
+  }
+  basic_navigation();
+
 
 });
---
+++ partials/slide_navigation.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-light ">
+  <div class="container nav_main--wrapper">
+    <div class="brand_and_btn--wrapper">
+      <a class="navbar-brand" href="index.php">
+        <img src="img/logo.svg" class="img-fluid" alt="logo">
+      </a>
+      <button class="navbar-toggler" type="button" >
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <div class="" id="navbarNav">
+        <ul class="navbar-nav container">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Features</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+  </div>
+
+
+</nav>
