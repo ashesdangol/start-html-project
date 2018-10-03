c8dd6d6963a7acb6d10727ff432345e1994031e1
ashesdangol <ashesdangol@hotmail.com>
2018-10-03 19:53:33 +0545

    zoom effect on banner pics

--- partials/header.php
+++ partials/header.php
@@ -4,12 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Nepal's Web Design &amp; Web Devloper in Kathmandu, Lalitpur. Develop the front-end web page by using HTML5 CSS3, SASS, JQuery, Javascript, bootstrap, php and Laravel.
+    Responsive Design. Web Sites is fully responsive to all kinds of devices such as ipad, iphone, android, desktop and big screens. Work with browser compatibility issues. Version Control using GIT. Optimize and make HTML WebPage SEO Friendly">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap-reboot.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" />
     <link rel="stylesheet" href="slick/slick.css" />
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <link rel="stylesheet" href="css/animate.css" />
     <link rel="stylesheet" href="css/main_stylesheet.css" />
-    <title>Ashes</title>
+    <title>Ashes - Build Personal Website</title>
   </head>
   <body>
