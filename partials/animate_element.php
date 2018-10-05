6a92dddfd697aea771627134d5d7c980f2a08b19
ashesdangol <ashesdangol@hotmail.com>
2018-10-05 16:59:10 +0545

    rotating cube

--- partials/animate_element.php
+++ partials/animate_element.php
@@ -15,6 +15,7 @@
       </div>
     </div>
     <div class="row">
+      <!--marble 3d  -->
       <div class="col-sm-6 col-md-4 col-xl-3">
         <div class="animate_element--wrapper">
           <div class="animate_element">
@@ -25,12 +26,13 @@
           </div>
         </div>
       </div>
+      <!--flying ballong 3d -->
       <div class="col-sm-6 col-md-4 col-xl-3">
         <div class="animate_element--wrapper">
           <div class="animate_element">
             <div class="animate__bg--outer sky--img" data-tilt>
               <span class="animate_bg--inner">
-                
+
               </span>
 
 
@@ -38,12 +40,40 @@
           </div>
         </div>
       </div>
+      <!--cube 3d  -->
       <div class="col-sm-6 col-md-4 col-xl-3">
         <div class="animate_element--wrapper">
           <div class="animate_element">
-            <figure class="three__d--img">
-              <!-- <img src="img/marblewala.png" class="img-fluid" alt="marble" data-tilt data-tilt-scale="1.1" data-tilt-axis="x" > -->
-            </figure>
+            <div class="cube__main">
+              <div class="cube__box">
+                <div class="front__cube--face">
+                  <p>front</p>
+
+                </div>
+                <div class="back__cube--face">
+                  <p>back</p>
+
+                </div>
+                <div class="left__cube--face">
+                  <p>left</p>
+
+                </div>
+                <div class="right__cube--face">
+                  <p>right</p>
+
+                </div>
+                <div class="top__cube--face">
+                  <p>top</p>
+
+                </div>
+                <div class="bottom__cube--face">
+                  <p>bottom</p>
+
+                </div>
+
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
