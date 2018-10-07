81dee60e6c7a6c7fffe573e1daf57d654434443f
ashesdangol <ashesdangol@hotmail.com>
2018-10-07 18:33:07 +0545

    svg on scroll

--- partials/animate_element.php
+++ partials/animate_element.php
@@ -77,12 +77,16 @@
           </div>
         </div>
       </div>
-      <div class="col-sm-6 col-md-4 col-xl-3">
-        <div class="animate_element--wrapper">
+      <div class="col-sm-6 col-md-4 col-xl-3 svg__height">
+        <div class="animate_element--wrapper svg__bg">
           <div class="animate_element">
-            <figure class="three__d--img">
-              <!-- <img src="img/marblewala.png" class="img-fluid" alt="marble" data-tilt data-tilt-scale="1.1" data-tilt-axis="x" > -->
-            </figure>
+            <div class="svg__wrapper">
+              <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300.75 272.03"><title>coding</title><path d="M212.8,239.48a4.8,4.8,0,0,0-6.57-1.66L150.47,271a4.78,4.78,0,0,0,0,8.22l55.76,33.22a4.8,4.8,0,0,0,6.56-1.66,4.79,4.79,0,0,0-1.66-6.57l-48.86-29.1,48.86-29.1a4.8,4.8,0,0,0,1.67-6.57Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/><path d="M299.21,310.82a4.8,4.8,0,0,0,6.56,1.66l55.76-33.22a4.78,4.78,0,0,0,0-8.22l-55.76-33.22a4.79,4.79,0,0,0-4.9,8.23l48.86,29.1-48.86,29.1A4.79,4.79,0,0,0,299.21,310.82Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/><path d="M274.33,239.26,229.72,305.7a4.79,4.79,0,0,0,7.95,5.33l44.61-66.43a4.79,4.79,0,0,0-7.95-5.34Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/><path d="M401.21,120.36H110.79a4.79,4.79,0,0,0-4.79,4.79v261.7a4.79,4.79,0,0,0,4.79,4.79H401.21a4.79,4.79,0,0,0,4.79-4.79V335.79a4.79,4.79,0,1,0-9.57,0v46.27H115.57V168.23H396.43V316.64a4.79,4.79,0,1,0,9.57,0V125.15A4.79,4.79,0,0,0,401.21,120.36Zm-285.64,9.58H292.7v28.72H115.57Zm186.71,28.72V129.94h94.15v28.72Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/><path d="M349.51,139.19a5.11,5.11,0,1,0,5.11,5.11A5.12,5.12,0,0,0,349.51,139.19Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/><path d="M323.34,139.19a5.11,5.11,0,1,0,5.11,5.11A5.11,5.11,0,0,0,323.34,139.19Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/><path d="M375.68,139.19a5.11,5.11,0,1,0,5.11,5.11A5.12,5.12,0,0,0,375.68,139.19Z" transform="translate(-105.63 -119.99)" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="0.75"/>
+              </svg>
+
+
+
+            </div>
           </div>
         </div>
       </div>
--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,6 @@
   <!-- jquery cdn -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
+  <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script> -->
+  <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
   <!-- jQuery local fallback -->
   <script>window.jQuery || document.write('<script src="js/jquery-3.3.1.js"><\/script>')</script>
 
@@ -18,6 +19,7 @@
       }
     });
   </script>
+  <script src="plugins/drawsvg.js"></script>
   <script src="plugins/tilt.jquery.js"></script>
   <script src="slick/slick.min.js"></script>
   <script src="js/custom.js"></script>
